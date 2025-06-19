15b6e6afc3569b65c89c9875d065748398c2d214
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-06-19 22:06:20 +0500

    JS DAY 2

--- javaScript/day1.js
+++ javaScript/day1.js
@@ -1,10 +1,9 @@
-console.log("Hello, Worldd")
+console.log("Hello, Worldd");
 
 var number1 = 10;
 
 var number2 = 20;
 
-var sumOfTwoNumbers = number1 + number2
+var sumOfTwoNumbers = number1 + number2;
 
-
-console.log(sumOfTwoNumbers)
+console.log(sumOfTwoNumbers);
---
+++ javaScript/day2.js
@@ -0,0 +1,53 @@
+var firstName = "Muhammad";
+var lastName = "Zohaib";
+
+var fullName = firstName + lastName;
+
+console.log(fullName);
+
+//TYPES IN JS
+
+// STRING
+// NUMBER
+// BOOLEAN
+// NULL
+// UNDEFINED
+// OBJECT
+// ARRAY
+
+var price = 1000;
+console.log(price + 100);
+
+// console.log(age);
+
+var todayDate = false;
+
+var dob = null;
+
+console.log(typeof dob, "dob");
+t;
+
+var age;
+console.log(age);
+
+var studentData = {
+  name: "Jawwad",
+  age: 26,
+};
+
+var fruitsData = {
+  name: "orange",
+  type: "winter fruit",
+};
+
+var name = "Jaawad";
+var age = 26;
+
+console.log(typeof studentData);
+
+var fruitsName = [studentData, studentData];
+
+var num1 = "100";
+var num2 = "20";
+
+console.log(num1 - num2);
